20475f0e1a76d4d240e1499cc4b74d578e77910c
Lucas Eduardo dos Santos de Souza <lueduardo2001@hotmail.com.br>
2023-08-26 18:20:51 -0300

    Some doc tutorials

---
+++ Login Screen.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+class LoginScreen(GridLayout):
+    ''' O método __init__ é um constructor chamado sempre que um objeto é
+    criado. Esse processo se chama override, onde substitui o método da
+    classe mãe. É recomendado usar o parâmetro **kwargs, pois pode haver
+    usos internos. '''
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MyApp().run()
